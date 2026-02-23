4977be0e36c9f61aa7b8262581987cd4fc21456e
Bailey <baileythueson@gmail.com>
2026-02-23 11:59:16 -0700

    Began SRE work to implement Codex API

---
+++ StoryMode.API/Controllers/CodexController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using StoryMode.Models;
+
+namespace StoryMode.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CodexController : ControllerBase
+{
+    private static readonly List<CodexEntry> _mockDb = new()
+    {
+        new CodexEntry()
+        {
+            Id = 1,
+            Name = "Gandalf (SRE CHECK)",
+            JsonData = "{ \"status\": \"You shall not crash\" }"
+        }
+    };
+
+    private readonly ILogger<CodexController> _logger;
+    
+    public CodexController(ILogger<CodexController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public ActionResult<List<CodexEntry>> Get()
+    {
+        _logger.LogInformation("Codex accessed from Kubernetes!");
+        return _mockDb;
+    }
+    
+    [HttpGet("{id}")]
+    public IEnumerable<CodexEntry> Get(int id)
+    {
+        _logger.LogInformation($"Retrieving Codex entry with ID: {id}");
+        return _mockDb;
+    }
+
+    [HttpGet("stress")]
+    public IActionResult Stress()
+    {
+        // simulate slow db query
+        Thread.Sleep(500);
+        return Ok("That was heavy...");
+    }
+}
